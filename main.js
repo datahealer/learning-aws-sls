7ddaa065883cb451c27b290de9d6e8b65a3eede5
Paras Kumar <itskparas@gmail.com>
2023-02-16 12:45:15 +0530

    added feature of sending welcome mail and a feature to send mail to user whenever we want

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const connectDB = require("./db");
 const Empolyee = require("./schema");
+require("dotenv").config();
+
+const AWS = require("aws-sdk");
+
+const awsConfig = {
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  region: process.env.region,
+};
+
+const SES = new AWS.SES(awsConfig);
 
 connectDB();
 
@@ -15,25 +26,96 @@ module.exports.getEmployees = async (event) => {
 module.exports.postEmployee = async (event) => {
   try {
     const parsedEmpolyeData = JSON.parse(event.body);
-    const { name, department } = parsedEmpolyeData;
+    const { name, department, email } = parsedEmpolyeData;
     console.log(name, department);
     const employee = new Empolyee({
       name,
       department,
+      email,
     });
     const empoly = await employee.save();
+    const fromEmail = process.env.from;
+    const params = {
+      Source: fromEmail,
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Subject: {
+          Data: `Welcome To inzint`,
+        },
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: `<h1>Thanks ${name} for joining Our Organization.</h1>`,
+          },
+        },
+      },
+    };
+
+    const messageId = await SES.sendEmail(params)
+      .promise()
+      .then((data) => data.MessageId);
+
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: "user saved",
         data: empoly,
+        message: "email sent succesfully",
+        messageId,
       }),
     };
   } catch (error) {
     return {
       statusCode: 501,
       body: JSON.stringify({
-        message: err.message,
+        message: error.message,
+      }),
+    };
+  }
+};
+
+module.exports.sentMailToEmployee = async (event) => {
+  try {
+    const parsedEmpolyeData = JSON.parse(event.body);
+    const { message, email } = parsedEmpolyeData;
+
+    const fromEmail = process.env.from;
+    const params = {
+      Source: fromEmail,
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Subject: {
+          Data: `mail From paras`,
+        },
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: `<h1>${message}</h1>`,
+          },
+        },
+      },
+    };
+
+    const messageId = await SES.sendEmail(params)
+      .promise()
+      .then((data) => data.MessageId);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "email sent succesfully",
+        messageId,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 501,
+      body: JSON.stringify({
+        message: error.message,
       }),
     };
   }
--- schema.js
+++ schema.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const empolyeeSchema = new mongoose.Schema({
   name: String,
   department: String,
+  email: {
+    type: String,
+    unique: true,
+  },
 });
 
 // creating Schema
